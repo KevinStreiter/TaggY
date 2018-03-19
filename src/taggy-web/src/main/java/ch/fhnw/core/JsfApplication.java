5fbf8306b6877ccef762bd3ce819a8bf5dc00d48
Joel Zimmerli <zimmi.94@live.de>
2018-03-19 14:30:19 +0100

    Packaging

--- src/taggy-web/src/main/java/ch/fhnw/core/JsfApplication.java
+++ src/taggy-web/src/main/java/ch/fhnw/core/JsfApplication.java
@@ -2,9 +2,16 @@ package ch.fhnw.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JsfApplication {
+public class JsfApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JsfApplication.class);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(JsfApplication.class, args);
